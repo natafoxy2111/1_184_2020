d3514d06a362095d7916943f749f82445a45d763
Dmitrii <presnovdmitriy02@gmail.com>
2020-11-03 22:39:54 +0300

    LABWORK5

---
+++ LABWORK5/LABWORK5.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double  x;
+	
+
+	
+
+	for (double x = 0; x<= 5  ; x=x+0.2)
+	{
+		double e=2.712828;
+
+		double num = (x * x) + cbrt(x + 1) + pow(e, -x);
+		double denum = sin(x + 2 * x + 3);
+		double y = num / denum;
+		cout << "x=" << x << "|   " << "y=" << y << endl;
+	}
+	return 0;
+}
+
---
+++ LABWORK5/LABWORK5.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double x;
+	cout << "VVEDITE X=" ;
+	cin>> x;
+	double s = 0;
+	for (int i = 4; i <= 20; i++)
+	{
+		
+		double e = 2.72818;
+		double num = (log(i * x) / log(e)) * (log(i * x) / log(e));
+		double denum = i + x;
+		double y = num / denum;
+		 s = s + y;
+		cout << "x=" << x << "|    " <<"i="<<i<<"|    "<< "y=" << y <<"|    "<<"s="<<s<< endl;
+	}
+	cout << "SUMMA=" << s << endl;
+	return 0;
+	
+	
+	
+	
+	}
+
+
+
+
