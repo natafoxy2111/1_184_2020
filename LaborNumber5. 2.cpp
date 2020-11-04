7c6e0eeb7871cac9180c85640403576ce43dd17e
natafoxy2111 <70664421+natafoxy2111@users.noreply.github.com>
2020-11-04 21:37:31 +0300

    Numb2

---
+++ LaborNumber5. 2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+const double pi = 3.14;
+int main() {
+	double pi = 1;
+	for (double i = 1; i <= 10; i = i++) {
+		pi = pi * log(abs(cos(i)));
+	}
+	cout << "F= " << pi;
+	return 0;
+}
+
