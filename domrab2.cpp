2e587f360a8451b355831cd291fb2bbf37aac4cc
Aleksandra771 <71434163+Aleksandra771@users.noreply.github.com>
2020-11-04 16:21:47 +0300

    Update domrab2.cpp

--- domrab2.cpp
+++ domrab2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main() {
 	double p = 1;
-	for (double i = 1; i < 15; i = i + 2) {
+	for (double i = 1; i <= 15; i = i + 2) {
 		p = p * (1 + exp(-cos(i)));
 	}
 	cout << "P = " << p;
