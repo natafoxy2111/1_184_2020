5f91e47a7e4dc93725210800f5d8af03cde5dcd6
Igor <ikurbatov@mail.ru>
2020-11-05 16:36:33 +0300

    лабораторная

---
+++ лаб.ном5вар10(1).cpp
@@ -0,0 +1,23 @@
+﻿// лаб.ном5вар10(1).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+double function(double xmin, double xmax, double dx)
+{
+
+	for (double x = xmin; x <= xmax + dx; x += dx)
+	{
+		double y = ( cos ( x ) + sin ( x * x )) / (pow(x, 3) +exp(x));
+
+		std::cout << " X = " << x << ", Y = " << y << std::endl;
+	}
+	return 0;
+}
+
+int main()
+{
+	function(1, 3, 0.1);
+	return 0;
+}
---
+++ лаб.ном5вар10(2).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double funct()
+{
+	double y = 1;
+
+	for (int k = 3; k <= 15; k = k + 3)
+	{
+		y = y * (1 + exp(sin(k)));
+	}
+
+	cout << y;
+	return 0;
+}
+
+int main()
+{
+	funct();
+	return 0;
+}
