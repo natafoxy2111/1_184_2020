b840f0d49d277b273fd039823d2e8c44447f23a2
natafoxy2111 <70664421+natafoxy2111@users.noreply.github.com>
2020-11-15 22:57:14 +0300

    Nat1

---
+++ VolodinaTowers.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void hanoy(int m, int a, int b, int c)
+{
+    if (m == 0) return;
+    hanoy(m - 1, a, c, b);
+    cout << a << " => " << b << endl;
+    hanoy(m - 1, c, b, a);
+}
+
+void main()
+{
+    int n = 3, x = 1, y = 2, z = 3;
+    hanoy(n, x, z, y);
+    cin.get();
+    return ;
+}
