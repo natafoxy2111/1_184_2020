0b91bcea692385750a51613b7fcb70a60b712dc8
natafoxy2111 <70664421+natafoxy2111@users.noreply.github.com>
2020-12-07 18:59:29 +0300

    Nata324

---
+++ Child.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Child {
+private:
+	string name;
+	string play;
+	string sleep;
+	string eat;
+public:
+	Child(string n, string s, string e, string p) {
+		name = n; sleep = s; eat = e, play = p;
+	}
+	void display() {
+		cout << "Name: " << name << endl;
+		cout << eat << endl;
+		cout << sleep << endl;
+		cout << play << endl;
+		cout << endl;
+	}
+
+};
+int main() {
+	Child Misha = Child("Misha", "want eat", "", "");
+	Misha.display();
+	Child Roma = Child("Roma", "want sleep", "", "");
+	Roma.display();
+	return 0;
+
+}
