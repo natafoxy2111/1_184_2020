6c63dff3dc6cf7143e4395bac90b6c9f93dd6b91
Nikita-Uzhastin <fosfor20@mail.ru>
2020-12-09 19:18:40 +0300

    HomeWork

---
+++ About MyWorld.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+class MyWorld
+{
+private:
+	string NameofThing;
+	string MaterialOfThing;
+	string SizeOfThing;
+public:
+	MyWorld(string NameOfThing, string MaterialOfThing, string SizeOfThing)
+	{
+		this->NameofThing = NameOfThing;
+		this->MaterialOfThing = MaterialOfThing;
+		this->SizeOfThing = SizeOfThing;
+	}
+
+	void Display()
+	{
+		cout << "Thing >> " << NameofThing << endl << "Material of " << NameofThing << " >> " << MaterialOfThing << endl << "Size of " << NameofThing << " >> " << SizeOfThing;
+	}
+
+	~MyWorld()
+	{
+	}
+};
+
+int main()
+{
+	MyWorld Table("Table", "wood", "Medium");
+	Table.Display();
+	cout << endl;
+	MyWorld Armchair("Armchair", "wood", "small");
+	Armchair.Display();
+
+	return 0;
+}
