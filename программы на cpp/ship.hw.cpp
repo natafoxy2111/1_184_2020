e5e86fa7123ff698fefe574174bf98a24177a8fa
Kseniya <71325356+KseniyaShilova@users.noreply.github.com>
2020-12-14 23:17:26 +0300

    Add files via upload

---
+++ программы на cpp/ship.hw.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Ship {
+private:
+	string name;
+	string length;
+	long long displacement;
+public:
+	Ship(string n, string r, long long p) {
+		this->name = n;
+		this->length = r;
+		this->displacement = p;
+	}
+	Ship(string n, long  long p) {
+		this->name = n;
+		this->length = "34";
+		this->displacement = p;
+	}
+	void vessel() {
+		cout << "This ship's name is " << name << ". The length of this ship " << length << ". Its displacement " << displacement << "." << endl;
+	}
+	void  Picture() {
+		string picture = R""""(
+     /""""""""\
+|||||||||||||||||||
+ \               /
+  """""""""""""""
+)"""";
+		cout << picture << endl;
+	}
+	void Change(long long p) {
+		this->displacement = p;
+	}
+	~Ship() {}
+};
+int main() {
+	Ship one("Titanik", "269", 52310);
+	Ship two("Kevin Mary", "311", 78638);
+	one.vessel();
+	two.vessel();
+	two.Change(114147);
+	two.vessel();
+	two.Picture();
+	return 0;
+}
