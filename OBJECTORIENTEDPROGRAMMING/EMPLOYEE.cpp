61a5310296f7ca1e9f2d9f4cdc4235c8400a703f
DMITRII <presnovdmitriy02@gmail.com>
2020-12-18 14:18:28 +0300

    oop

--- OBJECTORIENTEDPROGRAMMING/EMPLOYEE.cpp
+++ OBJECTORIENTEDPROGRAMMING/EMPLOYEE.cpp
@@ -2,40 +2,50 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Baby 
+class EMPLOYEE 
 {
-private:
+public:
 	string name;
-	string eat;
-	string sleep;
-	string play;
-	Baby(string n,string e,string s, string d)
+	string surname;
+	string patronymic;
+	int ident;
+	string data;
+	EMPLOYEE(string s,string n,string p, int a, string d)
 	{
-		name = n; eat = e; sleep = s; play = p;
+		surname = s; name = n; ident = a; data = d;   patronymic=p;
 	}
 	void display()
 	{
 		cout << "_____________________________" << endl;
-		cout << "NAME:  " << name << " ";
-		cout << eat << endl;
-		cout << sleep << endl;
-		cout << play << endl;
+		cout << "FULL NAME:  " << surname << " ";
+		cout << name << " ";
+		cout<< patronymic << endl;
+		cout << "IDENT " << ident << endl;
+		cout << "DATE: " << data << endl;
 		cout << "_____________________________" << endl;
 		cout << endl;
 	}
-	~Baby() {};
+	~EMPLOYEE() {};
 };
 int main()
 {
-	Baby ONE = Baby("PAUL","","want sleep","");
-	Baby TWO = Baby("ALEX", "want eat", "want sleep", "");
-	Baby THREE = Baby("RU", "want eat", "", "want play");
-	Baby FOUR = Baby("DAN", "", "", "want play");
-	
+	EMPLOYEE ONE = EMPLOYEE("LOPPY","D.","L.",22,"08/09/15");
+	EMPLOYEE TWO = EMPLOYEE("HERRANY", "A.", "R.", 21, "18/10/14");
+	EMPLOYEE THREE = EMPLOYEE("MILAN", "S.", "J.", 19, "06/07/19");
+	EMPLOYEE FOUR = EMPLOYEE("POOL", "P.", "Q.", 41, "01/09/04");
+	EMPLOYEE FIVE = EMPLOYEE("POTO", "E.", "O.", 28, "03/11/14");
+	EMPLOYEE SIX = EMPLOYEE("OSSO", "O.", "L.", 32, "28/10/12");
+	EMPLOYEE SEVEN = EMPLOYEE("ALEXEEV", "I.", "A.", 23, "21/03/10");
+	EMPLOYEE EIGHT = EMPLOYEE("ROND", "T.", "E.", 24, "26/01/14");
+	EMPLOYEE NINE = EMPLOYEE("LU", "P.", "K.", 36, "01/10/10");
 	ONE.display();
 	TWO.display();
 	THREE.display();
 	FOUR.display(); 
-	
+	FIVE.display(); 
+	SIX.display(); 
+	SEVEN.display();
+	EIGHT.display();
+	NINE.display();
 	return 0;
 }
