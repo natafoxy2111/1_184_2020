70a23ffd98710ba8b562f44ec6ea5c90011b9e61
Наталья Володина <70664421+natafoxy2111@users.noreply.github.com>
2021-01-10 15:05:25 +0300

    Add files via upload

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Person
+{
+public:
+    string name;
+    int age;
+    Person(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+    Person(string n)
+    {
+        name = n;
+        age = 18;
+    }
+    Person()
+    {
+        name = "Natasha";
+        age = 18;
+    }
+    void move()
+    {
+        cout << name << " is moving" << endl;
+    }
+};
+int main()
+{
+    Person Dasha("Dasha", 18);
+    cout << "Name: " << Dasha.name << "\tAge: " << Dasha.age << endl;
+    Person Masha("Masha", 20);
+    cout << "Name: " << Masha.name << "\tAge: " << Masha.age << endl;
+    Person Tata = Person();
+    cout << "Name: " << Tata.name << "\tAge: " << Tata.age << endl;
+    return 0;
+}
